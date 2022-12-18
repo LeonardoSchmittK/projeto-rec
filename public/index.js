e9d973e2f276778c753fdb3b207247f9955369d7
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2022-12-18 16:58:34 -0300

    input cleaner

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 let todoListAlreadyExists = false;
 const numberOfItemsEl = document.querySelector(".number-of-items")
+const mainBtn = document.querySelector(".btn-add-items")
 const textInput = document.querySelector("input[type=text].todo-input")
 textInput.focus()
 let textInputValue = textInput.value
@@ -15,13 +16,13 @@ function printStoredList(){
     todoListContainer.innerHTML=""
     todoListStorage.map((todoItem,id)=>{
           todoListContainer.innerHTML+=`
-          <div class="todoItem-container">
+          <li class="todoItem-container">
             <input ${todoItem.checked?"checked":""} onclick="markItem(this.id)" type="checkbox" name="" id="${id}">
             <h1 class="${todoItem.checked?"doneItem":""}">${todoItem.item}</h1>
             <button class="remove-button" id="${id}" onclick="removeItem(this.id)">
             <i class="fa-solid fa-trash"></i>
             </button>
-          </div>
+          </li>
             `
     })
   
@@ -73,7 +74,8 @@ function addItemToList(val){
     localStorage.todolist = JSON.stringify([...todoListStorage])
     printStoredList(todoListStorage)
     numberOfItemsEl.innerHTML=todoListStorage.length
-
+    textInput.value=""
+    textInput.focus()
 }
 
 function removeItem(id){
